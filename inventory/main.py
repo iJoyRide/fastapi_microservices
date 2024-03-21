538e28f405ce7aa6256a7d21f31ba985b3bd06e2
iJoyRide <sumfeis3@gmail.com>
2024-03-21 16:48:29 +0800

    fix

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
 
 app = FastAPI()
 
-
+redis = get_redis_connection()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
